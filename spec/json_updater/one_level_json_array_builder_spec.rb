51df855f60cc1fd3f7e3fe9043ec531f5696f615
Vladislav Hilko <vladislav.spawn@gmail.com>
2019-02-05 16:44:54 +0300

    Add tests for one level array builder

--- spec/json_updater/one_level_json_array_builder_spec.rb
+++ spec/json_updater/one_level_json_array_builder_spec.rb
@@ -10,27 +10,26 @@ describe JsonUpdater::OneLevelJsonArrayBuilder do
       let(:expected_file)  { json_file("#{base_path}/add_fields/expected.json")}
 
       it 'adds addition fields to the json file' do
-        # binding.pry
         expect(described_class.build(changable_file, etalon_file)).to eq expected_file
       end
     end
 
-    # context 'when fields were removed' do
-    #   let(:etalon_file)    { json_file("#{base_path}/remove_fields/etalon.json") }
-    #   let(:expected_file)  { json_file("#{base_path}/remove_fields/expected.json")}
-    #
-    #   it 'removes addition fields from the json file' do
-    #     expect(described_class.update_json(changable_file, etalon_file)).to eq expected_file
-    #   end
-    # end
-    #
-    # context 'when fields were removed and were added' do
-    #   let(:etalon_file)    { json_file("#{base_path}/mixed_fields/etalon.json") }
-    #   let(:expected_file)  { json_file("#{base_path}/mixed_fields/expected.json")}
-    #
-    #   it 'adds and removes addition fields from the json file' do
-    #     expect(described_class.update_json(changable_file, etalon_file)).to eq expected_file
-    #   end
-    # end
+    context 'when fields were removed' do
+      let(:etalon_file)    { json_file("#{base_path}/remove_fields/etalon.json") }
+      let(:expected_file)  { json_file("#{base_path}/remove_fields/expected.json")}
+
+      it 'removes addition fields from the json file' do
+        expect(described_class.build(changable_file, etalon_file)).to eq expected_file
+      end
+    end
+
+    context 'when fields were removed and were added' do
+      let(:etalon_file)    { json_file("#{base_path}/mixed_fields/etalon.json") }
+      let(:expected_file)  { json_file("#{base_path}/mixed_fields/expected.json")}
+
+      it 'adds and removes addition fields from the json file' do
+        expect(described_class.build(changable_file, etalon_file)).to eq expected_file
+      end
+    end
   end
 end
