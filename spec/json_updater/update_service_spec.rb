b5117947aee1d48c42a81af03fbd0038f0aeccda
Vladislav Hilko <vladislav.spawn@gmail.com>
2019-02-06 14:20:35 +0300

    Add tests for working with multiple json level

--- spec/json_updater/update_service_spec.rb
+++ spec/json_updater/update_service_spec.rb
@@ -187,5 +187,51 @@ describe JsonUpdater::UpdateService do
         end
       end
     end
+
+    context 'when json has fields with multiple level of hash structure' do
+      let(:base_path)           { 'spec/fixtures/fields_with_multiple_level' }
+      let(:changable_file_path) { "#{base_path}/fields_with_multiple_level.json" }
+      let(:temp_path)           { "#{base_path}/temp.json" }
+
+      let!(:temp_file) { File.open(changable_file_path).read }
+
+      before do
+        File.open(temp_path, 'w') { |f| f.write(temp_file) }
+      end
+
+      after do
+        File.delete(temp_path)
+      end
+
+      context 'when fields were added' do
+        let(:etalon_file_path)    { "#{base_path}/add_fields/etalon.json" }
+        let(:expected_file_path)  { "#{base_path}/add_fields/expected.json" }
+
+        it 'adds addition fields to the json file' do
+          described_class.update_json(temp_path, etalon_file_path)
+          expect(File.open(temp_path).read).to eq File.open(expected_file_path).read.strip
+        end
+      end
+
+      context 'when fields were removed' do
+        let(:etalon_file_path)    { "#{base_path}/remove_fields/etalon.json" }
+        let(:expected_file_path)  { "#{base_path}/remove_fields/expected.json" }
+
+        it 'removes addition fields from the json file' do
+          described_class.update_json(temp_path, etalon_file_path)
+          expect(File.open(temp_path).read).to eq File.open(expected_file_path).read.strip
+        end
+      end
+
+      context 'when fields were removed and were added' do
+        let(:etalon_file_path)    { "#{base_path}/mixed_fields/etalon.json" }
+        let(:expected_file_path)  { "#{base_path}/mixed_fields/expected.json" }
+
+        it 'adds and removes addition fields from the json file' do
+          described_class.update_json(temp_path, etalon_file_path)
+          expect(File.open(temp_path).read).to eq File.open(expected_file_path).read.strip
+        end
+      end
+    end
   end
 end
